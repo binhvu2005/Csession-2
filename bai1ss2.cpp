9a65f967ffdb109332946ee8e9806c86e267d49d
binhvu2005 <147390616+binhvu2005@users.noreply.github.com>
2023-11-07 13:21:54 +0700

    Add files via upload

---
+++ bai1ss2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+	int canh_dai_hcn= 12;
+	int canh_ngan_hcn = 8;
+	int chu_vi_hcn = ((canh_dai_hcn+canh_ngan_hcn)*2);
+	int dien_tich_hcn=(canh_dai_hcn * canh_ngan_hcn);
+	int canh_hv= 10;
+	int chu_vi_hv = (canh_hv*4);
+	int dien_tich_hv = (canh_hv*canh_hv);
+	float PI=3.14;
+	int ban_kinh= 10;
+	float chu_vi_ht= (2*ban_kinh*PI);
+	float dien_tich_ht=(ban_kinh*ban_kinh*PI);
+}
---
+++ thuhanh1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h> 
+int main(){
+  int age;
+  bool sex;
+  float mark;
+  age = 18;
+  sex = true;
+  mark = 9.5;
+  char studentId[] = {'S', 'V', '0', '0', '1'};
+  char studentName[] = "Vu Thanh Binh";
+  double number1 = 2.3, number2 = 1.1, number3 = -7.3;
+}
